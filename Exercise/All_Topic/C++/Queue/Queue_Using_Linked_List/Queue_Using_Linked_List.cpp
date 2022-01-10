8380c23c02d5bec278f03071f9ee5f0c95274913
Roman Ojha <razzroman98@gmail.com>
2022-01-10 16:13:20 +0545

    queue using linked list

---
+++ Exercise/All_Topic/C++/Queue/Queue_Using_Linked_List/Queue_Using_Linked_List.cpp
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// making front and rare as a global variable
+struct Node *f = NULL;
+struct Node *r = NULL;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void linkedListTraversal(struct Node *ptr)
+{
+    printf("Printing the elements of this linked list\n");
+    while (ptr != NULL)
+    {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+void enqueue(int val)
+{
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+    if (n == NULL)
+    {
+        printf("Queue is Full");
+        // when the memory will full
+    }
+    else
+    {
+        n->data = val;
+        n->next = NULL;
+        if (f == NULL)
+        {
+            f = r = n;
+        }
+        else
+        {
+            r->next = n;
+            r = n;
+        }
+    }
+}
+
+int dequeue()
+{
+    int val = -1;
+    struct Node *ptr = f;
+    if (f == NULL)
+    {
+        printf("Queue is Empty\n");
+    }
+    else
+    {
+        f = f->next;
+        val = ptr->data;
+        free(ptr);
+    }
+    return val;
+}
+
+int main()
+{
+    linkedListTraversal(f);
+    printf("Dequeuing element %d\n", dequeue());
+    enqueue(34);
+    enqueue(4);
+    enqueue(7);
+    enqueue(17);
+    printf("Dequeuing element %d\n", dequeue());
+    printf("Dequeuing element %d\n", dequeue());
+    printf("Dequeuing element %d\n", dequeue());
+    printf("Dequeuing element %d\n", dequeue());
+    enqueue(17);
+    enqueue(17);
+    linkedListTraversal(f);
+
+    return 0;
+}
