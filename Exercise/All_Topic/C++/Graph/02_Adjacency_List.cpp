8a7d93163859c1bda3196ea5f66a65d7b654ae21
Roman Ojha <razzroman98@gmail.com>
2022-01-13 19:59:45 +0545

    BFS tut_86 concept

--- Exercise/All_Topic/C++/Graph/02_Adjacency_List.cpp
+++ Exercise/All_Topic/C++/Graph/02_Adjacency_List.cpp
@@ -1,7 +1,26 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
-int main(){
-    return 0;
-}
+struct AdjListNode
+{
+    // A structure to represent an adjacency list node
+    int dest;
+    struct AdjListNode *next;
+};
+
+struct AdjList
+{
+    // A structure to represent an adjacency list
+    struct AdjListNode *head;
+};
+
+// A structure to represent a graph. A graph
+// is an array of adjacency lists.
+// Size of array will be V (number of vertices in graph)
+struct Graph
+{
+    int V;
+    struct AdjList *arry;
+};
---
+++ Tutorial/Code_With_Harry_Tutorial/Tut_86_Breadth_First_Search_BFS_Graph_Traversal/Tut_86_Breadth_First_Search_BFS_Graph_Traversal.cpp
@@ -0,0 +1,9 @@
+/*
+    -> Graph Traversal refers to the proces of visiting (checking and/or updaing) each vertex(node) in a graph
+    -> Two Algorithms of Graph Traversal are:
+        -> Breadth First SEarch(BFS)
+        -> Depth First Search(DFS)
+    -> In BFS, we start with a node and start exploring its connected nodes. the same process is repeated with all the connecting nodes until all the nodes are visited
+
+    => 
+*/
