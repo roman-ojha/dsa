08a64c9723b398b5eef8469fce7e4814088faa8f
Roman-Ojha <razzroman98@gmail.com>
2022-07-25 00:04:40 +0545

    test(practice): implemented stack using linked list

--- Example/All_Topic/C++/Data_Structure/Stack/stack_using_linked_list.cpp
+++ Example/All_Topic/C++/Data_Structure/Stack/stack_using_linked_list.cpp
@@ -11,12 +11,16 @@ public:
     {
         data = d;
     }
+    int getData()
+    {
+        return data;
+    }
 };
 
 void push(Node **top, int data)
 {
     Node *new_node = new Node(data);
-    if (top == NULL)
+    if ((*top) == NULL)
     {
         new_node->next = NULL;
         (*top) = new_node;
@@ -26,8 +30,29 @@ void push(Node **top, int data)
     (*top) = new_node;
 }
 
+int pop(Node **top)
+{
+    if ((*top) == NULL)
+    {
+        cout << "Stack is empty" << endl;
+        return -1;
+    }
+    int data = (*top)->getData();
+    (*top) = (*top)->next;
+    return data;
+}
+
 int main()
 {
     Node *top = NULL;
+    push(&top, 10);
+    push(&top, 11);
+    push(&top, 17);
+    push(&top, 23);
+    cout << "Popping: " << pop(&top) << endl;
+    cout << "Popping: " << pop(&top) << endl;
+    cout << "Popping: " << pop(&top) << endl;
+    cout << "Popping: " << pop(&top) << endl;
+    cout << "Popping: " << pop(&top) << endl;
     return 0;
 }
