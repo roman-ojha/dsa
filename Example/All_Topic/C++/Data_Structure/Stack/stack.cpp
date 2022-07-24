749faf6c436c4775eb83c1f151cf4f2b4f51de3f
Roman-Ojha <razzroman98@gmail.com>
2022-07-24 09:34:21 +0545

    test(practice): Stack isEmpty, isFull, push, pop, topofStack

--- Example/All_Topic/C++/Data_Structure/Stack/stack.cpp
+++ Example/All_Topic/C++/Data_Structure/Stack/stack.cpp
@@ -12,6 +12,50 @@ public:
         size = s;
         top = -1;
     }
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        return false;
+    }
+    bool isFull()
+    {
+        if (top == size - 1)
+        {
+            return true;
+        }
+        return false;
+    }
+    void push(int data)
+    {
+        if (isFull())
+        {
+            cout << "Stack is full" << endl;
+            return;
+        }
+        arr[++top] = data;
+    }
+    int pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        top--;
+        return arr[top + 1];
+    }
+    int topOfStack()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        return arr[top];
+    }
 };
 
 int main()
