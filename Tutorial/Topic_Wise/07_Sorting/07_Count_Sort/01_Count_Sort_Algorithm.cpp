5518f8505f3bc84d5ffa8562a3d2142023ea1fc0
Roman-Ojha <razzroman98@gmail.com>
2023-01-06 14:25:43 +0545

    refactor: structured all sorting algorithm tutorial

--- Tutorial/Topic_Wise/07_Sorting/07_Count_Sort/01_Count_Sort_Algorithm.cpp
+++ Tutorial/Topic_Wise/07_Sorting/07_Count_Sort/01_Count_Sort_Algorithm.cpp
@@ -8,7 +8,7 @@
         so, maxVal=9 create new array of arr2[10]
         0,0,0,0,0,0,0,0,0
 
-    1) 
+    1)
         0,0,0,1,0,0,0,0,0,0 -> arr1[0]=3, so arr2[3]++;
         0,1,0,0,0,0,0,0,0,0 -> arr1[1]=1, so arr2[1]++;
         0,1,0,1,0,0,0,0,0,1 -> arr1[2]=9, so arr2[9]++;
@@ -42,86 +42,11 @@
         0,0,0,0,0,0,0,0,0,1 -> arr2[8]=0 -> move to next index
         0,0,0,0,0,0,0,0,0,1 -> arr2[9]=1 -> decrement 1--
               1,1,2,3,4,6,9 -> arr3[6]=9(index of arr2[9])
-        0,0,0,0,0,0,0,0,0,0 -> arr2[9]=0 
+        0,0,0,0,0,0,0,0,0,0 -> arr2[9]=0
               1,1,2,3,4,6,9 -> Sorted array
 
-            
+
     -> it will take extra space count[n+1]
     -> Time Complexity = O(m+n) or O(n) => Fastest Shorting algorithm
-        
+
 */
-
-#include <iostream>
-#include <stdio.h>
-#include <climits>
-#include <malloc.h>
-using namespace std;
-
-void printArray(int *A, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", A[i]);
-    }
-    printf("\n");
-}
-
-int maximum(int A[], int n)
-{
-    int max = INT_MIN;
-    for (int i = 0; i < n; i++)
-    {
-        if (max < A[i])
-        {
-            max = A[i];
-        }
-    }
-    return max;
-}
-
-void countSort(int *A, int n)
-{
-    int i, j;
-    // Find the maximum element in A
-    int max = maximum(A, n);
-    int *count = (int *)malloc((max + 1) * sizeof(int));
-    // Dynamically creating the array for the count
-
-    // Initialize the array element to 0
-    for (i = 0; i < max + 1; i++)
-    {
-        count[i] = 0;
-    }
-
-    // Incrementing the corresponding index in the count array
-    for (i = 0; i < n; i++)
-    {
-        count[A[i]] = count[A[i]] + 1;
-    }
-
-    i = 0; // counter for count array
-    j = 0; // counter for given array
-    while (i <= max)
-    {
-        if (count[i] > 0)
-        {
-            A[j] = i;
-            count[i] = count[i] - 1;
-            j++;
-        }
-        else
-        {
-            i++;
-        }
-    }
-}
-
-int main()
-{
-    int A[] = {9, 1, 4, 14, 4, 15, 6};
-    int n = 7;
-    printArray(A, n);
-    countSort(A, n);
-    printArray(A, n);
-    return 0;
-}
