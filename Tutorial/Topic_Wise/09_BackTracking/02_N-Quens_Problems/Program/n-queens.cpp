de0d6d2a87c9c0412c7b418332bd5d4da7c014ec
Roman-Ojha <razzroman98@gmail.com>
2023-01-06 11:31:55 +0545

    feat(n-queens): successfully find all the possible solution & print the all boards

--- Tutorial/Topic_Wise/09_BackTracking/02_N-Quens_Problems/Program/n-queens.cpp
+++ Tutorial/Topic_Wise/09_BackTracking/02_N-Quens_Problems/Program/n-queens.cpp
@@ -26,10 +26,76 @@ using namespace std;
 // Store board of all the possible solution
 vector<vector<string>> allBoard;
 
-// 'isSafe' function will check does it will safe to place Queen in the given place
-int safe(int row, int col, vector<string> &board)
+// Print all the possible solution board
+void print_board(int n)
 {
-    // Checking horizontal line
+    for (int i = 0; i < allBoard.size(); i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << allBoard[i][j] << endl;
+        }
+        cout << endl;
+    }
+}
+
+// 'isSafe' function will check does it will safe to place Queen in the given place
+int isSafe(int row, int col, vector<string> &board)
+{
+    // Checking horizontal line:
+    for (int j = 0; j < board.size(); j++)
+    {
+        if (board[row][j] == 'Q')
+            return false;
+    }
+
+    // Checking vertical lines:
+    for (int i = 0; i < board.size(); i++)
+    {
+        if (board[i][col] == 'Q')
+            return false;
+    }
+
+    // Checking diagonal lines:
+    // Top right diagonal:
+    // i-- & j++
+    int i = row, j = col;
+    while (i >= 0 && j < board.size())
+    {
+        // Moving on Top left diagonal element
+        if (board[i--][j++] == 'Q')
+            return false;
+    }
+    // Top left diagonal:
+    // i-- & j--
+    i = row, j = col;
+    while (i >= 0 && j >= 0)
+    {
+        // Moving on Top left diagonal element
+        if (board[i--][j--] == 'Q')
+            return false;
+    }
+    // Bottom right diagonal:
+    // i++ & j++
+    i = row, j = col;
+    while (i < board.size() && j < board.size())
+    {
+        // Moving on Top left diagonal element
+        if (board[i++][j++] == 'Q')
+            return false;
+    }
+    // Bottom left diagonal:
+    // i++ & j--
+    i = row, j = col;
+    while (i < board.size() && j >= 0)
+    {
+        // Moving on Top left diagonal element
+        if (board[i++][j--] == 'Q')
+            return false;
+    }
+
+    // now it will safe to place
+    return true;
 }
 
 // Recursive function to solve nQueens problem
@@ -81,15 +147,8 @@ vector<vector<string>> solveNQueens(int n)
 
 int main()
 {
-    // int n;
-    // cout << "Enter N no. of Queens you want to place: ";
-    // cin >> n;
-    // Dynamically create n*n size of array
-    // string *board = new string[n * n];
-    // board[i][j] = board[i*n+j]
-
-    // NOTE: rather we will chose 4*4 size of board for simplicity
-    // string board[4][4];
-
+    solveNQueens(4);
+    cout << "No. of possible solution is: " << allBoard.size() << endl;
+    print_board(4);
     return 0;
 }
