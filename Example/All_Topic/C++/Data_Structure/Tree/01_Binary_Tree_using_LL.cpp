c71c02b28907c1f19a7f1937abc3780a6e8c734a
Roman-Ojha <razzroman98@gmail.com>
2023-01-20 14:37:05 +0545

    feat: BuildTree of Binary Tree

--- Example/All_Topic/C++/Data_Structure/Tree/01_Binary_Tree_using_LL.cpp
+++ Example/All_Topic/C++/Data_Structure/Tree/01_Binary_Tree_using_LL.cpp
@@ -17,6 +17,49 @@ class BT
 {
 public:
     Node<Data> *root;
+    BT()
+    {
+        this->root = nullptr;
+    }
+    BT(Data data)
+    {
+        Node<Data> *node = new Node<Data>(data);
+        this->root = node;
+    }
+
+    void insert(Data data)
+    {
+        if (this->root == nullptr)
+        {
+            // Create root node
+            Node<Data> *node = new Node<Data>(data);
+            this->root = node;
+        }
+        else
+        {
+            // Insert node level wise
+            Node<Data> *node = new Node<Data>(data);
+
+            // Preorder traversal and then insert
+            // Node<Data> *ptr = this->root;
+            while (true)
+            {
+                if (ptr->left == nullptr)
+                {
+                    ptr->left = node;
+                    break;
+                }
+                else if (ptr->right == nullptr)
+                {
+                    ptr->right = node;
+                    break;
+                }
+
+                ptr = ptr->left;
+                ptr = ptr->right;
+            }
+        }
+    }
 };
 
 int main()
---
+++ Tutorial/Topic_Wise/08_Tree/64_Build_Binary_Tree/Build_Binary_Tree.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <class Data>
+class Node
+{
+public:
+    Node<Data> *left;
+    Data data;
+    Node<Data> *right;
+    Node(Node<Data> *left, Data data, Node<Data> *right) : left(left), data(data), right(right) {}
+    Node(Data data) : data(data), left(nullptr), right(nullptr) {}
+};
+
+template <class Data>
+class BT
+{
+public:
+    Data index = -1;
+    Node<Data> *build(vector<Data> *nodes)
+    {
+        this->index++;
+        if (nodes->at(this->index) == -1)
+        {
+            // When it is a leaf node we will return nullptr
+            return nullptr;
+        }
+        else
+        {
+            // It is a node
+            Node<Data> *node = new Node<Data>(nodes->at(index));
+            node->left = build(nodes);
+            node->right = build(nodes);
+
+            // In the end it will return the root node
+            return node;
+        }
+    }
+
+    void inOrder(Node<Data> *ptr)
+    {
+        if (ptr == nullptr)
+        {
+            return;
+        }
+        // Left
+        inOrder(ptr->left);
+        // Root
+        cout << ptr->data << " ";
+        // Right
+        inOrder(ptr->right);
+    }
+};
+
+int main()
+{
+    vector<int> nodes{1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    BT<int> bt;
+    Node<int> *root = bt.build(&nodes);
+    bt.inOrder(root);
+    return 0;
+}
