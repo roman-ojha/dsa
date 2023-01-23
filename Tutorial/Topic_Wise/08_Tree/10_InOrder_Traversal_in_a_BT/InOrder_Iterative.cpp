2b09f069b22215318bdcf6a29bf0b52e5995359f
Roman-Ojha <razzroman98@gmail.com>
2023-01-23 22:55:50 +0545

    feat: done InOrder traversal iteratively

---
+++ Tutorial/Topic_Wise/08_Tree/10_InOrder_Traversal_in_a_BT/InOrder_Iterative.cpp
@@ -0,0 +1,97 @@
+// https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion/
+/*
+    1) Create an empty stack S.
+    2) Initialize current node as root
+    3) Push the current node to S and set current = current->left until current is NULL
+    4) If current is NULL and stack is not empty then
+        a) Pop the top item from stack.
+        b) Print the popped item, set current = popped_item->right
+        c) Go to step 3.
+    5) If current is NULL and stack is empty then we are done.
+*/
+
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+template <class Data>
+class Node
+{
+public:
+    Node<Data> *left;
+    Data data;
+    Node<Data> *right;
+    Node(Node<Data> *left, Data data, Node<Data> *right) : left(left), data(data), right(right) {}
+    Node(Data data) : data(data), left(nullptr), right(nullptr) {}
+};
+
+template <class Data>
+class BT
+{
+public:
+    Data index = -1;
+    Node<Data> *build(vector<Data> *nodes)
+    {
+        this->index++;
+        if (nodes->at(this->index) == -1)
+        {
+            // When it is a leaf node we will return nullptr
+            return nullptr;
+        }
+        else
+        {
+            // It is a node
+            Node<Data> *node = new Node<Data>(nodes->at(index));
+            node->left = build(nodes);
+            node->right = build(nodes);
+
+            // In the end it will return the root node
+            return node;
+        }
+    }
+
+    void inOrder(Node<Data> *root)
+    {
+        std::stack<Node<Data> *> S;
+        Node<Data> *ptr = root;
+        S.push(ptr);
+        while (!S.empty())
+        {
+            if (ptr != nullptr)
+            {
+                // Left
+                ptr = ptr->left;
+                if (ptr != nullptr)
+                {
+                    S.push(ptr);
+                }
+            }
+            else
+            {
+                // Root
+                Node<Data> *curr = S.top();
+                cout << curr->data << " ";
+                S.pop();
+
+                // Right
+                ptr = curr->right;
+                if (ptr != nullptr)
+                {
+                    S.push(ptr);
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    // -1 == nullptr
+    vector<int> nodes{1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    BT<int> bt;
+    Node<int> *root = bt.build(&nodes);
+    bt.inOrder(root);
+    // 4 2 5 1 3 6
+    return 0;
+}
--- Tutorial/Topic_Wise/08_Tree/17_Deletion_in_BST/01_Deletion_in_BST.cpp
+++ Tutorial/Topic_Wise/08_Tree/17_Deletion_in_BST/01_Deletion_in_BST.cpp
@@ -1,27 +1,27 @@
 /*
  # from geeksfrogeeks
 
-We have discussed BST search and insert operations. In this post, the delete operation is discussed. When we delete a node, three possibilities arise. 
-1) Node to be deleted is the leaf: Simply remove from the tree. 
+We have discussed BST search and insert operations. In this post, the delete operation is discussed. When we delete a node, three possibilities arise.
+1) Node to be deleted is the leaf: Simply remove from the tree.
 
               50                            50
            /     \         delete(20)      /   \
-          30      70       --------->    30     70 
-         /  \    /  \                     \    /  \ 
+          30      70       --------->    30     70
+         /  \    /  \                     \    /  \
        20   40  60   80                   40  60   80
-2) Node to be deleted has only one child: Copy the child to the node and delete the child 
+2) Node to be deleted has only one child: Copy the child to the node and delete the child
 
               50                            50
            /     \         delete(30)      /   \
-          30      70       --------->    40     70 
-            \    /  \                          /  \ 
+          30      70       --------->    40     70
+            \    /  \                          /  \
             40  60   80                       60   80
-3) Node to be deleted has two children: Find inorder successor of the node. Copy contents of the inorder successor to the node and delete the inorder successor. Note that inorder predecessor can also be used. 
+3) Node to be deleted has two children: Find inorder successor of the node. Copy contents of the inorder successor to the node and delete the inorder successor. Note that inorder predecessor can also be used.
 
               50                            60
            /     \         delete(50)      /   \
-          40      70       --------->    40    70 
-                 /  \                            \ 
+          40      70       --------->    40    70
+                 /  \                            \
                 60   80                           80
 
 */
@@ -36,14 +36,14 @@ We have discussed BST search and insert operations. In this post, the delete ope
         step 1 -> search the node
         step 2 -> Delete the node
         step 3 -> make parent->child node NULL
-    
+
     Case 2)
         after deleting the the node which is non leaf node
         in that case the place of deleted node will taken by:
             a) Inorder pre(predecessor) OR b) Inorder post(successor)
             Ex: Inorder: 1,2,3,4,5,6
                 -> if we want to delete '4' then it has to replace by either 3 or 5
-    Case 3) 
+    Case 3)
         Ex:Inorder: 1,3,4,6,7,8,10,13,14
 
         if we want to delete 8 then first we have to delete and replace by either 7 or 10
