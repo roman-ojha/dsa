4834a0b3c176bd29142cfb8edd2ae4ddc485483e
Roman-Ojha <razzroman98@gmail.com>
2023-01-23 14:33:32 +0545

    refactor: improved folder/file names of Tree related tutorials

--- Tutorial/Topic_Wise/08_Tree/12_Binary_Search_Tree/71_Binary_Search_Trees.cpp
+++ Tutorial/Topic_Wise/08_Tree/12_Binary_Search_Tree/71_Binary_Search_Trees.cpp
@@ -6,5 +6,5 @@
         2) All nodes of the right subtree are greater.
         3) left and Right subtrees are also BST
         4) There are no duplicate nodes
-        5) Inorder traversal of a BST gives an ascending sorted array.
+        5) InOrder traversal of a BST gives an ascending sorted array.
 */
