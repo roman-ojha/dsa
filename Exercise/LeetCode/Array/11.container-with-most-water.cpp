7de8190cc2549e5b6566a9af237a9edbc364b2a3
Roman-Ojha <razzroman98@gmail.com>
2023-01-27 13:10:39 +0545

    feat: solved 'container with most water' problem using two pointer

--- Exercise/LeetCode/Array/11.container-with-most-water.cpp
+++ Exercise/LeetCode/Array/11.container-with-most-water.cpp
@@ -1,3 +1,31 @@
+/*
+    -> https://leetcode.com/problems/container-with-most-water/description/
+    *) Container With Most Water
+        You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+        Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+        Return the maximum amount of water a container can store.
+
+        Notice that you may not slant the container.
+
+        Example 1:
+        Input: height = [1,8,6,2,5,4,8,3,7]
+        Output: 49
+        Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+        Example 2:
+        Input: height = [1,1]
+        Output: 1
+
+        Constraints:
+        n == height.length
+        2 <= n <= 105
+        0 <= height[i] <= 104
+*/
+
+// https://leetcode.com/problems/container-with-most-water/solutions/127443/container-with-most-water/
+
 #include <bits/stdc++.h>
 using namespace std;
 /*
@@ -10,8 +38,7 @@ using namespace std;
 class Solution
 {
 public:
-    // Todo.. two pointer approach
-    int maxArea(vector<int> &height)
+    int maxArea1(vector<int> &height)
     {
         // using Brute force approach
         // time complexity = O(n^2)
@@ -39,6 +66,35 @@ public:
             }
         }
         return maxWater;
+    }
+
+    // using two pointer approach
+    int maxArea(vector<int> &height)
+    {
+        int left = 0, right = height.size() - 1;
+        int maxContainer = 0;
+        while (left < right)
+        {
+            int distance = right - left;
+            int tempMax = 0;
+            if (height[left] <= height[right])
+            {
+                // if right height is greater then calculate area according to left height
+                tempMax = distance * height[left];
+                // because left height is smaller we have to move towards rightwards to find bigger height
+                left++;
+            }
+            else
+            {
+                // if left height is greater then calculate area according to right height
+                tempMax = distance * height[right];
+                // because right height is smaller we have to move backwards from right to find bigger height
+                right--;
+            }
+            maxContainer = tempMax > maxContainer ? tempMax : maxContainer;
+        }
+
+        return maxContainer;
     }
 };
 // @lc code=end
