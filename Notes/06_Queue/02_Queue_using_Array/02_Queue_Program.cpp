990528147ad731d373876b0c44acbacd3786464e
Roman-Ojha <razzroman98@gmail.com>
2023-01-27 11:19:51 +0545

    refactor: improved file/folder strocture of Queue Notes

--- Notes/06_Queue/02_Queue_using_Array/02_Queue_Program.cpp
+++ Notes/06_Queue/02_Queue_using_Array/02_Queue_Program.cpp
@@ -1,3 +1,26 @@
+/*
+    Implementation of Queue:
+
+    struct Queue
+    {
+        int size;
+        int f; // front
+        int r; // rear
+        int *arr;
+    }
+
+    int main()
+    {
+        struct Queue q;
+        q.size=10;
+        q.f=-1;
+        q.r=-1;
+        q.arr=(int *)malloc(q.size*sizeof(int));
+        return 0;
+    }
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
