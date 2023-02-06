963800a873f3f19c6e1ab201c15af888205caeef
Roman-Ojha <razzroman98@gmail.com>
2023-02-06 15:42:52 +0545

    docs: related to Prim's algorithm for MST

--- Notes/11_Graph/11_Spanning_Tree_and_MST/Spanning_Tree.cpp
+++ Notes/11_Graph/11_Spanning_Tree_and_MST/Spanning_Tree.cpp
@@ -4,4 +4,6 @@
         -> And also it should follow the Tree property, means it could not have a cycle
         => Total possible number of spanning tree from K(complete) graph:
             -> n^(n - 2)
+        => More:
+            -> https://www.geeksforgeeks.org/prims-minimum-spanning-tree-mst-greedy-algo-5/
 */
---
+++ Notes/11_Graph/12_Prims_Algorithm_for_MST/Prims_Algoritm.cpp
@@ -0,0 +1,10 @@
+/*
+    *) Prim's Algorithm
+        -> It is a greedy algorithm
+        -> It is use to find the minimum cost spanning tree
+        -> While creating the minumum spanning tree we have to note that in every checking the minimum weight of an edge we have to consider the previous edges that we left while creating MST
+        -> While making MST using prim's algorithm It always will be connected and It should not be cyclic
+        => we can solve minimum spanning Tree using:
+            -> priority queue
+            -> Hash map
+*/
