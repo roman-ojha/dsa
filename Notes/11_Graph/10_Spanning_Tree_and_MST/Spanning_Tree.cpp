4119274a728c104f957326ade48213197223f74b
Roman-Ojha <razzroman98@gmail.com>
2023-02-06 15:09:02 +0545

    docs: related to Spanning Tree & MST

---
+++ Notes/11_Graph/10_Spanning_Tree_and_MST/Spanning_Tree.cpp
@@ -0,0 +1,7 @@
+/*
+    *) Spanning Tree
+        -> It is a Connected Subgraph
+        -> And also it should follow the Tree property, means it could not have a cycle
+        => Total possible number of spanning tree from K(complete) graph:
+            -> n^(n - 2)
+*/
