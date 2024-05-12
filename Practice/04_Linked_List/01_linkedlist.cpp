0a16fdf82635107a2b3c819d002cb48b5c353dc2
Roman-Ojha <80277272+roman-ojha@users.noreply.github.com>
2024-05-12 15:17:43 +0545

    feat(linked-list): completed all insertion method

--- Practice/04_Linked_List/01_linkedlist.cpp
+++ Practice/04_Linked_List/01_linkedlist.cpp
@@ -11,10 +11,63 @@ public:
 };
 
 class LinkedList {
-  Node head;
+  Node *head;
 
 public:
-  Node *insert(int data, Node *head) {}
+  LinkedList(int data) { this->head = new Node(data); }
+
+  void insertAtEnd(int data) {
+    Node *newNode = new Node(data);
+    newNode->next = NULL;
+    Node *ptr = this->head;
+    while (ptr->next != NULL) {
+      ptr = ptr->next;
+    }
+    ptr->next = newNode;
+  }
+
+  void insertAtBeginning(int data) {
+    Node *newNode = new Node(data);
+    Node *ptr = this->head;
+    newNode->next = ptr;
+    this->head = newNode;
+  }
+
+  void insertInBetween(int data, int position) {
+    Node *newNode = new Node(data);
+    Node *ptr = this->head;
+    for (int i = 1; i < position - 1; i++) {
+      if (ptr != NULL)
+        ptr = ptr->next;
+      else
+        std::cout << "Given position doesn't exist in linkedlist" << std::endl;
+    }
+    Node *tempPtr = ptr->next;
+    ptr->next = newNode;
+    newNode->next = tempPtr;
+  }
+
+  void traversel() {
+    Node *ptr = this->head;
+    while (ptr != NULL) {
+      std::cout << ptr->data << " -> ";
+      ptr = ptr->next;
+    }
+    std::cout << "NULL" << std::endl;
+  }
 };
 
-int main() { return 0; }
+int main() {
+  LinkedList *l = new LinkedList(10);
+  l->traversel();
+  l->insertAtEnd(13);
+  l->traversel();
+  l->insertAtBeginning(21);
+  l->insertAtBeginning(32);
+  l->insertAtEnd(15);
+  l->insertInBetween(51, 3);
+  l->insertInBetween(33, 3);
+  l->insertAtBeginning(03);
+  l->traversel();
+  return 0;
+}
